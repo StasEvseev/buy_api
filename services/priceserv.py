6374e1a16b1b5d676ddcd13658063cd3ba595f76
StasEvseev <stasevseev@gmail.com>
2014-10-16 10:22:54 +0400

    recommendation price with warning on_change

--- services/priceserv.py
+++ services/priceserv.py
@@ -8,7 +8,8 @@ from sqlalchemy.orm.exc import NoResultFound
 
 
 PriceStub = namedtuple('PriceStub', ['id_commodity', 'full_name', 'number_local', 'number_global', 'NDS', 'price_prev',
-                                     'price_post', 'price_retail', 'price_gross'])
+                                     'price_post', 'price_retail', 'price_gross', 'is_change',
+                                     'price_retail_recommendation', 'price_gross_recommendation'])
 
 
 class PriceServiceException(Exception):
@@ -53,22 +54,49 @@ class PriceService(object):
 
     @classmethod
     def get_price_to_commodity(cls, id_commodity):
-        return Price.query.filter(
+        price = Price.query.filter(
             Price.commodity_id==id_commodity,
-        ).order_by(desc(Price.number_local), desc(Price.number_global)).one()
+        ).order_by(desc(Price.number_local), desc(Price.number_global)).first()
+        if price is None:
+            price = PriceStub(
+            id_commodity=id_commodity,
+            full_name='',
+            number_local='',
+            number_global='',
+            NDS='',
+            price_prev='',
+            price_post='',
+            price_retail='',
+            price_gross='',
+            price_retail_recommendation='',
+            price_gross_recommendation='',
+            is_change=''
+        )
+        return price
 
     @classmethod
     def generate_price_stub(cls, products):
 
-        return [
-            PriceStub(
-                id_commodity=CommodityService.get_commodity(prod.name).id,
+        res = []
+
+        for prod in products:
+            commodity = CommodityService.get_commodity(prod.name)
+            price = PriceService.get_price_to_commodity(commodity.id)
+
+            pricestub = PriceStub(
+                id_commodity=commodity.id,
                 full_name=prod.full_name,
                 number_local=prod.number_local,
                 number_global=prod.number_global,
                 NDS=prod.rate_NDS,
                 price_prev=prod.price_without_NDS,
                 price_post=prod.price_with_NDS,
-                price_retail=PriceService.get_price_to_commodity(CommodityService.get_commodity(prod.name).id).price_retail,
-                price_gross=PriceService.get_price_to_commodity(CommodityService.get_commodity(prod.name).id).price_gross) for prod in products
-        ]
+                price_retail=price.price_retail or '',
+                price_gross=price.price_gross or '',
+                price_retail_recommendation=float(prod.price_with_NDS) * 1.6,
+                price_gross_recommendation=float(prod.price_with_NDS) * 1.4,
+                is_change=((price.price_prev and prod.price_without_NDS != price.price_prev) or (price.price_post and prod.price_with_NDS != price.price_post)))
+
+            res.append(pricestub)
+
+        return res
