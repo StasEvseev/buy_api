2ea5ca995e8702d4ea5d50722306fd6032cc81c7
StasEvseev <stasevseev@gmail.com>
2014-10-16 12:34:38 +0400

    Warining alert

--- admin/myview.py
+++ admin/myview.py
@@ -13,21 +13,28 @@ class MyView(BaseView):
     @expose('/prices/<string:invoice_id>')
     def prices(self, invoice_id):
         url_back = url_for('.index')
-        url_next = url_for('.invoice_retail', invoice_id=invoice_id)
+        url_invoice_retail = url_for('.invoice_retail', invoice_id=invoice_id)
+        url_invoice_gross = url_for('.invoice_gross', invoice_id=invoice_id)
         invoice = MailInvoiceService.get_invoice(invoice_id)
 
         items = PriceService.generate_price_stub(invoice.items)
 
         return self.render('prices.html',
                            date=invoice.date,
+                           attention=any([it.is_change for it in items]),
                            url_back=url_back,
-                           url_next=url_next,
+                           url_retail=url_invoice_retail,
+                           url_gross=url_invoice_gross,
                            invoice_items=items)
 
     @expose('/invoice-retail/<string:invoice_id>')
     def invoice_retail(self, invoice_id):
         return self.render('invoice_retail.html')
         pass
+
+    @expose('/invoice-gross/<string:invoice_id>')
+    def invoice_gross(self, invoice_id):
+        return self.render('invoice_gross.html')
 
     @expose('/bla/bla')
     def bla(self):
